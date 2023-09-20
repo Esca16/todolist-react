8399e0e2c08d0c29a17b73f283bf21a84c9617a5
Esca16 <znh1684@gmail.com>
2023-09-20 11:58:54 +0630

    added edit button

--- src/components/ToDoList/ToDoItem.jsx
+++ src/components/ToDoList/ToDoItem.jsx
@@ -1,27 +1,69 @@
-import React, { useContext } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { ToDoContext } from '../../context/ToDoProvider'
-import { RiEdit2Fill, RiDeleteBin2Fill } from "react-icons/ri";
+import { RiEdit2Fill, RiDeleteBin2Fill, RiCheckFill, RiCloseFill } from "react-icons/ri";
 
-const ToDoItem = ({todo}) => {
-  const { deleteToDo } = useContext(ToDoContext);
+const ToDoItem = ({ todo }) => {
+  const { deleteToDo, editToDo } = useContext(ToDoContext);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const refEditInput = useRef(null);
 
   const deleteHandler = () => {
     deleteToDo(todo.id)
   }
+
+  const EditHandler = () => {
+    setIsEditMode(true);
+    setIsReadOnly(false);
+    refEditInput.current.focus();
+  }
+
+  const CancelHandler = () => {
+    setIsEditMode(false);
+    setIsReadOnly(true);
+  }
+
+  const SaveChangeHandler = () => {
+    setIsEditMode(false);
+    setIsReadOnly(true);
+    todo.text = refEditInput.current.value;
+    editToDo(todo);
+  };
+
+  const CompleteCheckHandler = (e) => {
+    console.log(e.target.checked);
+    todo.completed = e.target.checked;
+    editToDo(todo)
+  }
+
   return (
     <div className='flex mt-4 justify-between items-center w-full bg-slate-400 py-4 px-5'>
       <div className='flex gap-6 items-center'>
-        <input type="checkbox" name="chk-completed" id="chk-completed" defaultChecked={todo.completed ? true : false} />
-        <p className='text-lg text-white'>{todo.text}</p>
+        <input type="checkbox" name="chk-completed" id="chk-completed" onClick={(e) => CompleteCheckHandler(e)} defaultChecked={todo.completed ? true : false} />
+        <input type="text" ref={refEditInput} defaultValue={todo.text} readOnly={isReadOnly} className={`outline-none bg-transparent w-full ${!isEditMode && todo.completed ? "line-through text-gray-200" : ""}`} onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            SaveChangeHandler();
+          }
+        }} />
       </div>
-      <div>
-        <button className='mr-4'>
-          <RiEdit2Fill size={22} className="text-yellow-300" />
-        </button>
-        <button>
-          <RiDeleteBin2Fill onClick={deleteHandler} size={22} className="text-red-500" />
-        </button>
-      </div>
+      {isEditMode ? (
+        <div>
+          <button className="mr-4" onClick={SaveChangeHandler}>
+            <RiCheckFill size={22} className="text-green-400" />
+          </button>
+          <button onClick={CancelHandler}>
+            <RiCloseFill size={22} className="text-red-500" />
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button className='mr-4'>
+            <RiEdit2Fill onClick={EditHandler} size={22} className="text-yellow-300" />
+          </button>
+          <button>
+            <RiDeleteBin2Fill onClick={deleteHandler} size={22} className="text-red-500" />
+          </button>
+        </div>)}
     </div>
   )
 }
--- src/context/ToDoProvider.jsx
+++ src/context/ToDoProvider.jsx
@@ -1,31 +1,21 @@
 // import React from 'react'
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export const ToDoContext = createContext();
 
 const ToDoProvider = ({ children }) => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: "Hi",
-            completed: true
-        },
-        {
-            id: 2,
-            text: "Hey",
-            completed: false
-        },
-        {
-            id: 3,
-            text: "Hello",
-            completed: false
-        }
-    ]);
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        const IsToDos = JSON.parse(localStorage.getItem("st_r_d"));
+        IsToDos && setTodos([...IsToDos]);
+    }, [])
 
     const addToDo = (newTodo) => {
         newTodo.id = uuidv4();
+        localStorage.setItem("st_r_d", JSON.stringify([...todos, newTodo]));
         setTodos([...todos, newTodo])
     };
 
@@ -33,9 +23,21 @@ const ToDoProvider = ({ children }) => {
         console.log(todoID);
         const filterToDo = todos.filter((todo) => todo.id != todoID);
         setTodos([...filterToDo])
+        localStorage.setItem("st_r_d", JSON.stringify([...filterToDo]));
+    };
+
+    const editToDo = (todo) => {
+        const editedToDo = todos.map((td) => {
+            if (td.id == todo.id) {
+                return todo;
+            } return td;
+        });
+
+        setTodos([...editedToDo]);
+        localStorage.setItem("st_r_d", JSON.stringify([...editedToDo]))
     }
     return (
-        <ToDoContext.Provider value={{ todos, addToDo, deleteToDo }}>{children}</ToDoContext.Provider>
+        <ToDoContext.Provider value={{ todos, addToDo, deleteToDo, editToDo }}>{children}</ToDoContext.Provider>
     )
 }
 
